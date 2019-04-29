60bafad6e52b29b2385e1ddabbcfee1e2fc75f9a
wadie <wadiesoyass@gmail.com>
2019-04-29 02:08:52 +0200

    Spring Security . JWT =Creation JWTAuthentification

--- src/main/java/com/ncs/masterAuto/security/JWTAuthentificationFilter.java
+++ src/main/java/com/ncs/masterAuto/security/JWTAuthentificationFilter.java
@@ -5,9 +5,14 @@
  */
 package com.ncs.masterAuto.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ncs.masterAuto.domain.bean.Client;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.FilterChain;
@@ -26,25 +31,37 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  * @author wadie
  */
 public class JWTAuthentificationFilter extends UsernamePasswordAuthenticationFilter {
-
+    
     @Autowired
     private AuthenticationManager authenticationManager;
-
+    
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         try {
             Client client = new ObjectMapper().readValue(request.getInputStream(), Client.class);
             return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(client.getAdresseMail(), client.getPwd()));
-
+            
         } catch (IOException ex) {
             Logger.getLogger(JWTAuthentificationFilter.class.getName()).log(Level.SEVERE, null, ex);
         }
         throw new RuntimeException("pb dans le contenue de la requete");
     }
-
+    
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult); //To change body of generated methods, choose Tools | Templates.
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        Client client = (Client) authResult.getPrincipal();
+        List<String> roles = new ArrayList<>();
+        authResult.getAuthorities().forEach(a -> {
+            roles.add(a.getAuthority());
+        });
+        String jwt = JWT.create()
+                .withIssuer(request.getRequestURI())
+                .withSubject(client.getNom())
+                .withArrayClaim("roles", roles.toArray(new String[roles.size()]))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 24 * 3600))
+                .sign(Algorithm.HMAC256("soyas"));
+        response.addHeader("Authorization",jwt );
     }
-
+    
 }
--- src/main/java/com/ncs/masterAuto/security/SecurityConfig.java
+++ src/main/java/com/ncs/masterAuto/security/SecurityConfig.java
@@ -22,25 +22,26 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+    
     @Autowired
     private UserDetailsService userDetailsService;
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-
+    
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
-
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-      http.authorizeRequests().antMatchers("/clients/**");
-        http.authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests().antMatchers("/clients/**", "/").permitAll();
+        http.authorizeRequests().antMatchers("/clients/**").hasAuthority("Admin");
+//        http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(new JWTAuthentificationFilter());
-
+        
     }
-
+    
 }
